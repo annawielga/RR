9982b439e4b531fcd90ee513e1f07db4cad1ea6f
annawielga <annawielga@gmail.com>
2017-01-23 21:18:01 +0000

    c1

---
+++ rrrrrrrrrrrrrrr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RR.MvcApplication" Language="C#" %>
---
+++ rrrrrrrrrrrrrrr/Models/Member.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RR.Models
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool GardaVetted { get; set; }
+        public bool Cox { get; set; }
+        public bool WeatherCaller { get; set; }
+
+        public override string ToString()
+        {
+            return this.MemberID+" "+this.Name;
+        }
+    }
+}
